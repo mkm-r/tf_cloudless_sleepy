5649fda0c1364ed4a42d8ef549fa457be4e5998e
bsvihovec <bsvihovec@yahoo.com>
2021-07-21 13:16:37 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -10,8 +10,34 @@ variable "sleepy_time" {
   default     = 0
 }
 
-variable "string_array" {
+variable "string_array_with_type" {
+  type = list(string)
 }
 
-variable "a_boolean" {
+variable "boolean_with_type" {
+  type = boolean
+}
+
+varaible "string_array_no_type" {
+  default = ["a", "b", "c"] 
+}
+
+variable "boolean_no_type" {
+  default = false
+}
+
+variable "object_array_no_type" {
+ default = [{"a":1}, {"b":2}] 
+}
+
+variable "object_array_with_type" {
+  type = list(object)
+}
+
+variable "object_with_type" {
+  type = object
+}
+
+variable "object_no_type" {
+  default = {"a":1}
 }
