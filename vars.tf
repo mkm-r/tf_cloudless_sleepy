e2b44bcb87d57c1348b9d1cf6516a39655ae5815
bsvihovec <bsvihovec@yahoo.com>
2021-07-22 22:55:18 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,5 +1,10 @@
 # Configure these variables
 
+variable "TF_VERSION" {
+  default = "0.14"
+  description = "terraform engine version to be used in schematics"
+}
+
 variable "sample_var" {
   description = "A sample_var to pass to the template."
   default     = "hello"
