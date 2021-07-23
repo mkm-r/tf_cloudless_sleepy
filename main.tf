c48dee1524da8f3887aba3800b0d28559d6c59b1
bsvihovec <bsvihovec@yahoo.com>
2021-07-23 09:51:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ resource "ibm_cm_offering" "cm_offering1" {
     test_bool = var.boolean_with_type
     test_map = var.object_with_type
 }
-resource "ibm_cm_offering" "cm_offering2" {
-    catalog_id = ibm_cm_catalog.cm_catalog.id
-    label = "tf_test_offering2"
-    tags = var.string_array_no_type
-    test_int = var.int_no_type
-    test_float = var.float_no_type
-    test_bool = var.boolean_no_type
-    test_map = var.object_no_type
-}
+# resource "ibm_cm_offering" "cm_offering2" {
+#     catalog_id = ibm_cm_catalog.cm_catalog.id
+#     label = "tf_test_offering2"
+#     tags = var.string_array_no_type
+#     test_int = var.int_no_type
+#     test_float = var.float_no_type
+#     test_bool = var.boolean_no_type
+#     test_map = var.object_no_type
+# }
