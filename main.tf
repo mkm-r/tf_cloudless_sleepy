9a00577bda5f63c771d18a0b9f05cc38fe6a3e73
Mujeebur <55774447+mkm-r@users.noreply.github.com>
2021-08-24 11:53:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,23 @@ resource "ibm_cm_offering" "cm_offering1" {
     test_bool = var.boolean_with_type
     test_map = var.object_with_type
 }
+
+output "resource_cloud" {
+  value = {
+
+    "is_resource" = "true"
+
+    "resource_controller_url" = ""
+
+    "resource_icon_url" = "https://globalcatalog.test.cloud.ibm.com/api/v1/1082e7d2-5e2f-0a11-a3bc-f88a8e1931fc/artifacts/cache/8c6189a76c7d346f6e807aaffb3bf643-public/terraform.svg"
+
+    "resource_id" = "b636d651-8489-4425-bd6a-f30af1603577.f054506e-a53b-402c-a948-b2f43f3560d1"
+
+    "resource_name" = "itemname"
+
+  }
+}
+
 # resource "ibm_cm_offering" "cm_offering2" {
 #     catalog_id = ibm_cm_catalog.cm_catalog.id
 #     label = "tf_test_offering2"
